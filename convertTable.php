59b88c5cddd4e30af4302240908eb89af485aac1
Viktor94 <vedasagar94@gmail.com>
2015-04-11 02:29:59 +0530

    Removed unnecessary files

--- convertTable.php
+++
@@ -1,9 +0,0 @@
-<?php
-$chkbox = $_POST['chk'];
-$txtbox = $_POST['txt'];
-$country = $_POST['country'];
- 
-foreach($txtbox as $a => $b)
-  echo "{ $inputCriteriaType[$a] : {description: $inputDescription[$a],  min  :  $inputMinVal[$a], max  :  $inputMaxVal[$a] }}";
- 
-?>
